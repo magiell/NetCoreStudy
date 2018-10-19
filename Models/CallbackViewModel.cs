e2f28e95bf6a033b45935e45fffa2b792ffc355a
igaw_cyan <cyan@igaworks.com>
2018-10-19 15:38:11 +0900

    first commit

---
+++ Models/CallbackViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication3.Models
+{
+    public class CallbackViewModel
+    {
+        public DateTime? CachedTime { get; set; }
+        public string Message { get; set; }
+    }
+}
